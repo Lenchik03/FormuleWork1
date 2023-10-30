e1977aeb6104bc2295bb912b362ab6ce5341524c
Polchik <pyrina200052004@mail.ru>
2023-10-30 19:50:05 +1000

    Добавьте файлы проекта.

---
+++ FormuleWork1/Program.cs
@@ -0,0 +1,105 @@
+﻿//задача 2
+//double a, b, c, d, result;
+//double.TryParse(Console.ReadLine(), out a);
+//double.TryParse(Console.ReadLine(), out b);
+//double.TryParse(Console.ReadLine(), out c);
+//double.TryParse(Console.ReadLine(), out d);
+
+//result = (a / c) * (b / d) - (a * b - c) / (c * d);
+//Console.WriteLine("Задача 1: " + result);
+
+//задача 3
+//double x,y,result;
+//double.TryParse(Console.ReadLine(), out x);
+//double.TryParse(Console.ReadLine(), out y);
+
+//result = (Math.Sin(x) + Math.Cos(y)) / (Math.Cos(x) - Math.Sin(y)) * Math.Tan(x*y);
+//Console.WriteLine(result);
+
+//задача 4
+//double x, y, result;
+//double.TryParse(Console.ReadLine(), out x);
+//double.TryParse(Console.ReadLine(), out y);
+
+//result = ((x+y)/(y+1))-((x*y-12)/(34+x));
+//Console.WriteLine(result);
+
+//задача 5
+//double x, y, result;
+//double.TryParse(Console.ReadLine(), out x);
+//double.TryParse(Console.ReadLine(), out y);
+
+//double a = Math.E;
+//result = (3+Math.Pow(a, y-1))/(1+Math.Pow(x,2)*Math.Abs(y-Math.Tan(x)));
+
+//Console.WriteLine(result);
+
+//задача 6
+//double x, result;
+//double.TryParse(Console.ReadLine(), out x);
+
+
+//result = x-(Math.Pow(x,3)/3)+(Math.Pow(x,5)/5);
+//Console.WriteLine(result);
+
+//задача 7
+//double x, y, result;
+//double.TryParse(Console.ReadLine(), out x);
+//double.TryParse(Console.ReadLine(), out y);
+
+//result = Math.Log(Math.Abs((y - Math.Sqrt(Math.Abs(x))) * (x - (y / (x + (Math.Pow(x, 2) / 4))))));
+//Console.WriteLine(result);
+
+//задача 8
+//double x, y, result;
+//double.TryParse(Console.ReadLine(), out x);
+//double.TryParse(Console.ReadLine(), out y);
+
+//result = Math.Pow(1-Math.Tan(x), 1/Math.Tan(x)) + Math.Cos(x-y);
+//Console.WriteLine(result);
+
+//задача 9
+//double x, result;
+//double.TryParse(Console.ReadLine(), out x);
+
+//result = Math.Log(Math.Abs(Math.Cos(x)))/Math.Log(1+Math.Pow(x,2));
+//Console.WriteLine(result);
+
+//задача 10
+//double x, y, result;
+//double.TryParse(Console.ReadLine(), out x);
+//double.TryParse(Console.ReadLine(), out y);
+
+//result = Math.Pow((x+1)/(x-1), x) + 18*x*Math.Pow(y,2);
+//Console.WriteLine(result);
+
+//задача 11
+//double x, y, result;
+//double.TryParse(Console.ReadLine(), out x);
+//double.TryParse(Console.ReadLine(), out y);
+
+//result = Math.Pow(1 + 1 / Math.Pow(x, 2), x) - 12*Math.Pow(x,2)*y;
+//Console.WriteLine(result);
+
+//задача 12
+//double x, result;
+//double.TryParse(Console.ReadLine(), out x);
+
+//result = (Math.Pow(x, 2) - 7 * x + 10) / (Math.Pow(x, 2) - 8 * x + 12);
+//Console.WriteLine(result);
+
+//задача 13
+//double x, y, result;
+//double.TryParse(Console.ReadLine(), out x);
+//double.TryParse(Console.ReadLine(), out y);
+
+//result = (Math.Cos(x) / (Math.PI) - 2 * x) + 16 * x * Math.Cos(x * y) - 2;
+//Console.WriteLine(result);
+
+//задача 14
+//double x, y, result;
+//double.TryParse(Console.ReadLine(), out x);
+//double.TryParse(Console.ReadLine(), out y);
+
+//result = Math.Pow(2, -x) - Math.Cos(x) + Math.Sin(2 * x * y);
+//Console.WriteLine(result);
